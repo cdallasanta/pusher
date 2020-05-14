9a2e0d42cb1a7c4538885f02e1ae1ef897cac413
cdallasanta <chris.dallasanta@gmail.com>
2020-05-14 12:52:22 -0700

    changed a file name and threw some stuff out of whack

---
+++ PusherRealtimeChat.WebAPI/Controllers/MessagesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PusherRealtimeChat.WebAPI.Models;
+using PusherServer;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace PusherRealtimeChat.WebAPI.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class MessagesController : ApiController
+    {
+        private static List<ChatMessage> messages =
+            new List<ChatMessage>()
+            {
+                new ChatMessage
+                {
+                    AuthorName = "Chris",
+                    Text = "Hello there."
+                },
+                new ChatMessage
+                {
+                    AuthorName = "Kristin",
+                    Text = "Hello back."
+                }
+            };
+
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(
+                HttpStatusCode.OK,
+                messages);
+        }
+
+        public HttpResponseMessage Post(ChatMessage message)
+        {
+            if (message == null || !ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(
+                    HttpStatusCode.BadRequest,
+                    "Invalid Input"
+                    );
+            }
+            messages.Add(message);
+
+            var pusher = new Pusher(
+                "1001149",
+                "c3645ffe81e6237e79ef",
+                "b6848be40e19184e03aa",
+                    new PusherOptions
+                    {
+                        Cluster = "us3"
+                    }
+                );
+            pusher.TriggerAsync(
+                channelName: "messages",
+                eventName: "new_message",
+                data: new
+                {
+                    AuthorName = message.AuthorName,
+                    Text = message.Text
+                });
+
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
---
+++ PusherRealtimeChat.WebAPI/Models/ChatMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PusherRealtimeChat.WebAPI.Models
+{
+    public class ChatMessage
+    {
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public string AuthorName { get; set; }
+    }
+}
